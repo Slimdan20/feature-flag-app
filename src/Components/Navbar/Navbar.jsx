4fba381b09b23871d793b8a503d75f427f439095
Daniel Efe <daneytechwriter@gmail.com>
2025-06-16 14:46:49 -0700

    new

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import imageOne from "../../assets/imageOne.png"
+import { useFlags } from 'flagsmith/react';
 
 const Navbar = () => {
+  const flags = useFlags(['dark-mode-feature']);
+  const darkModeEnabled = flags['dark-mode-feature']?.enabled;
+
+
   const [isActive, setIsActive] = useState(false);
 
   const toggleSwitch = () => {
@@ -9,6 +14,7 @@ const Navbar = () => {
   };
 
   useEffect(() => {
+    if (!darkModeEnabled) return;
     if (isActive) {
       document.body.style.background = "linear-gradient(to right, #0f2027, #203a43, #2c5364)";
       document.body.style.color = "white";
@@ -21,7 +27,7 @@ const Navbar = () => {
   navLinks.forEach(link => {
     link.style.color = isActive ? 'white' : 'black';
   });
-  }, [isActive]);
+  }, [isActive, darkModeEnabled]);
   
   return (
     <div>
@@ -34,14 +40,15 @@ const Navbar = () => {
         <a href="#">About</a>
         <a href="#">Contact</a>
         <a href="">Logout</a>
-       {/* <div className='darkmode'>
+        {darkModeEnabled && (<div className='darkmode'>
         <div className='first'>
           < i className='bxr  bx-brightness-half'  ></i> 
         </div>
         <div className={`second ${isActive ? 'active' : ''}`} onClick={toggleSwitch}>
           < i className='bxr  bxs-circle'  ></i> 
         </div>
-        </div>*/}
+        </div>
+)}
         </div>
       </nav>
     </div>
