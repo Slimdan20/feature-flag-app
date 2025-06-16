4f3203d5fcd1d8607f3d7c77dd2306d1e3ebfe2a
Daniel Efe <daneytechwriter@gmail.com>
2025-06-16 04:18:46 -0700

    new

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import imageOne from "../../assets/imageOne.png"
 
 const Navbar = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleSwitch = () => {
+    setIsActive(prev => !prev);
+  };
+
+  useEffect(() => {
+    if (isActive) {
+      document.body.style.background = "linear-gradient(to right, #0f2027, #203a43, #2c5364)";
+      document.body.style.color = "white";
+    } else {
+      document.body.style.background = "linear-gradient(to right, #b3e5fc, #81d4fa)";
+      document.body.style.color = "black";
+    }
+
+    const navLinks = document.querySelectorAll('nav a');
+  navLinks.forEach(link => {
+    link.style.color = isActive ? 'white' : 'black';
+  });
+  }, [isActive]);
+  
   return (
     <div>
       <nav>
         <div>
         <img src={imageOne} alt="" />
         </div>
-        <div>
+        <div className='head'>
         <a href="#">Home</a>
         <a href="#">About</a>
         <a href="#">Contact</a>
         <a href="">Logout</a>
+       {/* <div className='darkmode'>
+        <div className='first'>
+          < i className='bxr  bx-brightness-half'  ></i> 
+        </div>
+        <div className={`second ${isActive ? 'active' : ''}`} onClick={toggleSwitch}>
+          < i className='bxr  bxs-circle'  ></i> 
+        </div>
+        </div>*/}
         </div>
       </nav>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,20 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './AuthContext.jsx'
+import flagsmith from 'flagsmith'
+import {FlagsmithProvider} from 'flagsmith/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <FlagsmithProvider 
+    options={{environmentID: 'kdM3Aw6zL9jPuMvT9sPz4q'}}
+    flagsmith={flagsmith}
+    >
     <AuthProvider>
     <App />
     </AuthProvider>
+    </FlagsmithProvider>
     </BrowserRouter>
   </StrictMode>,
 )
